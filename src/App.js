501602e08f44089bf6f5d20ab9c7225800d1ff6a
mkdoh <mkdoherty3@gmail.com>
2021-01-06 16:39:11 -0700

    Creating a merge conflict

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-    return <div className="App"></div>;
+    return (
+        <div className="App">
+            <h1>This is the header I want</h1>
+            <h3>This is my subheading</h3>
+        </div>
+    );
 }
 
 export default App;
